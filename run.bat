7e3db6d3e300fe8b26c5ddfac022ae16b1ddfa76
AlekseyPanas <panasaleksey3@gmail.com>
2021-06-12 00:22:11 -0400

    update batch

--- run.bat
+++ run.bat
@@ -1,2 +1,12 @@
-python3 run.py
+@echo off
+pip3 > nul
+if %ERRORLEVEL%==9009 (
+	echo Running python
+	pip install pygame
+	python run.py
+) else (
+	echo Running python3
+	pip3 install pygame
+	python3 run.py
+)
 pause
